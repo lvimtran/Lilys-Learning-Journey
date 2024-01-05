a829f49022378dba4a8a485334e58f01fc5082c4
Lily <lvmytran@gmail.com>
2024-01-05 20:49:09 +1000

    Add course, complete an exercise

---
+++ JavascriptZeroToExpert/section3/ex5.js
@@ -0,0 +1,17 @@
+const calcAverage = (score1, score2, score3) => {
+    return (score1 + score2 + score3)/3
+}
+
+let scoreDolphins = calcAverage(44,23,71)
+let scoreKoalas = calcAverage(65,54,49)
+
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+};
