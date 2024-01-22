600eaee29a82c6047aaddef7e9832e16b674a1d9
Lily <lvmytran@gmail.com>
2024-01-22 21:52:16 +1000

    second commit

--- JavascriptZeroToExpert/section7/Guess-My-Number/script.js
+++ JavascriptZeroToExpert/section7/Guess-My-Number/script.js
@@ -11,11 +11,42 @@
 // //store value 23
 // document.querySelector(".guess").value = 23;
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
 
+  //No input
   if (!guess) {
     document.querySelector(".message").textContent = "No number!";
+
+    //Player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct number!";
+    document.querySelector("body").style.backgroundColor = "#60b367";
+
+    //Guess is higher
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+
+    //Guess is lower
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
