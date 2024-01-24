6a642981f1e5c83f9b80fa4a814fab176e458e34
Lily <lvmytran@gmail.com>
2024-01-25 09:51:56 +1000

    reset game

--- JavascriptZeroToExpert/section7/Pig-Game/script.js
+++ JavascriptZeroToExpert/section7/Pig-Game/script.js
@@ -14,15 +14,28 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-//Starting condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//Starting condition
+const init = function () {
+  const scores = [0, 0];
+  let currentScore = 0;
+  let activePlayer = 0;
+  let playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -36,7 +49,7 @@ const switchPlayer = function () {
 btnRoll.addEventListener("click", function () {
   if (playing) {
     //Generate random dice roll
-    const dice = Math.random(Math.random() * 6) + 1;
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
     //Display dice
     diceEl.classList.remove("hidden");
@@ -79,3 +92,5 @@ btnHold.addEventListener("click", function () {
     switchPlayer();
   }
 });
+
+btnNew.addEventListener("click", init);
